b780cc86e5560cdd0ace5143c297870fc1b8dced
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-04-06 18:56:31 +0100

    edit comments

--- pair_sum_-_sorted.py
+++ pair_sum_-_sorted.py
@@ -10,37 +10,43 @@ def pair_sum_sorted(nums: List[int], target: int) -> List[int]:
     Returns:
         a list of length 2. if no pair of indexes is found then list will be blank
     """
-    # Write your code here
+    # en
     #
     # ASSUMPTION
-    # the returned list must have unique indexes
-    # # example (not accepted) -> nums: [1, 2], target: 4 => [1, 1]
+    # - the returned list must have unique indexes.
+    # examples of expected behaviour:
+    #     nums: [1, 2], target: 4 => []
+    #     nums: [-1, 2], target: 1 => [0, 1]
+    # examples of not expected behaviour:
+    #     nums: [1, 2], target: 4 => [1, 1]
     #
     # APPROACH 00
-    # use 2 ptr starting from opposite ends
+    # use 2 pointers starting from opposite ends.
+    # move these pointers towards the center.
+    # calculate the sum for each given pair of points.
+    # if the sum is the target -> return the pair
+    # if the sum is not the target
+    #   is the sum less than the target?
+    #       move the lagging ptr up
+    #   is the sum greater than the target?
+    #       move the leading ptr down
+    #
+    # ANALYSIS
+    #   TIME:
+    #     O(n)
+    #     I must look at every value in the array at least once
+    #   SPACE:
+    #     O(1)
+    #     I only allocate the pointers and an array of fixed size to return
+    #
     i, j = 0, len(nums) - 1
-    # move these ptr towards the center
-    # for each given pair of points
     while i < j:
-        # # calculate the sum
         s = nums[i] + nums[j]
-        # #if the sum is target -> return the pair
         if s == target:
             return [i, j]
-        # # if the sum is not the target
         else:
-            # # # is the sum less than the target?
-            # # # // is the sum greater than the target?
             if s < target:
-                # # # # move the lagging ptr up
                 i = i + 1
             else:
-                # # # # move the leading ptr down
                 j = j - 1
-    #
-    # Complexity
-    # TIME: O(n):
-    # # we must look at every value in the array at least once
-    # SPACE: O(1):
-    # # we only allocate the pointers and array of fixed size for return
     return []

38a0596a8f3ca7aae42aa9770115ef55f4235ca0
scholar-of-artifice <will.fehlhaber.maker@gmail.com>
2025-04-08 22:23:24 +0100

    Create another is_palindrome_valid solution

--- is_palindrome_valid.py
+++ is_palindrome_valid.py
@@ -56,3 +56,40 @@ def is_palindrome_valid(s: str) -> bool:
     s = sanitize(s)
     s_r = ''.join(s[::-1])
     return s == s_r
+
+
+def is_palindrome_valid(s: str) -> bool:
+    """determines if a string is a palindrome.
+
+    Args:
+        s: a string
+    Returns:
+        true if the string is a palindrome.
+        false if the string is NOT a palindrome.
+    """
+    #
+    # ASSUMPTION
+    # a string is a palindrome if it can be read the same forwards and backwards.
+    #
+    # APPROACH 01
+    #
+    # santize an input string s.
+    # use 2 pointers that start at opposite ends of the input string.
+    # move them inwards.
+    # if characters do not match (at any iteration), then s is NOT a palindrome.
+    #
+    # ANALYSIS
+    #   TIME
+    #       - O(n)
+    #       - where n is the length of s.
+    #   SPACE
+    #       - O(1)
+    #       - the same number of pointers are allocated no matter the size of s.
+    #
+    s = sanitize(s)
+    i, j = 0, len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i, j = i + 1, j - 1
+    return True
